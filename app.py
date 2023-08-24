41cc26c9464e207271af48b581c1907505c097b0
lokyen <39927582+lokyen@users.noreply.github.com>
2023-08-24 19:28:24 +0000

    Implemented jinja temple tags and button

--- app.py
+++ app.py
@@ -2,9 +2,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Jujutsu Kaisen Vol 1', 
+    'genre': 'Supernatural',
+    'language': 'English',
+    'type': 'Manga',
+  },
+  {
+    'id': 2,
+    'title': 'Jujutsu Kaisen Vol 2', 
+    'genre': 'Supernatural',
+    'language': 'English',
+    'type': 'Manga',
+  },
+  {
+    'id': 3,
+    'title': 'Kaodake ja Suki ni Narimasen Vol 1', 
+    'genre': 'Slice of Life',
+    'language': 'Japanese',
+    'type': 'Manga',
+  },
+  {
+    'id': 4,
+    'title': 'Yubisaki to Renren vol 1', 
+    'genre': 'Slice of Life',
+    'language': 'Japanese',
+    'type': 'Manga',
+  },
+  
+]
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, app_name='Book Shelf')
 
 print(__name__)
 if __name__ == "__main__":
